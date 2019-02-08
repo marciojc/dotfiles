24465ac0309a2b6938312edd40394c9f1c1307be
marciojc <marciojc@gmail.com>
2019-02-08 10:10:44 +0000

    Add brew script

---
+++ homebrew/brew.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+
+# Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
+brew install bash
+
+brew install bash-completion
+
+brew install homebrew/completions/brew-cask-completion
+
+# Install wget with IRI support
+brew install wget --with-iri
+
+# Install more recent versions of some OS X tools
+brew install vim --with-override-system-vi
+brew install homebrew/dupes/nano
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+# Install other useful binaries
+
+brew install git
+brew install terminal-notifier
+
+brew install zsh
+
+# Remove outdated versions from the cellar
+brew cleanup
