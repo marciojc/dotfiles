cf051134a597bbd4e777438cdc926e7d9fe2957f
marciojc <marciojc@gmail.com>
2019-02-11 21:40:23 +0000

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,23 @@
 
 echo "Installing dotfiles."
 
+echo "Brew..."
 ./homebrew/install.sh
 ./homebrew/brew.sh
 ./homebrew/brew-cask.sh
+
+echo "Git..."
+cp git/gitconfig.symlink ~/.gitconfig
+cp git/gitignore_global.symlink ~/.gitignore
+
+echo "Zsh..."
+./zsh/oh-my-zsh.sh
+cp zsh/.aliases ~/.aliases
+cp zsh/.functions ~/.functions
+cp zsh/.theme ~/.theme
+cp zsh/.theme ~/.theme
+cp zsh/.zprofile ~/.zprofile
+cp zsh/.zshrc.symlink ~/.zshrc
+
+echo "Sublime..."
+./sublime/sublime.sh
