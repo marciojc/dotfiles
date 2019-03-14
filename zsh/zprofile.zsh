c2da26c374bf6ad097db6a714dc0645da853648c
marciojc <468890+marciojc@users.noreply.github.com>
2019-03-14 10:17:02 +0000

    Add laradock path

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -85,3 +85,5 @@ HISTDUP=erase               #Erase duplicates in the history file
 setopt    appendhistory     #Append history to the history file (no overwriting)
 setopt    sharehistory      #Share history across terminals
 setopt    incappendhistory  #Immediately append to the history file, not just when a term is killed
+
+export LARADOCK="$HOME/Sites/laravel/laradock/"
